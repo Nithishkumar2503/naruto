20ccdee70b67b2554a56c5e34039c897ca914104
Nithish <nithishkumar.shanmugam@airscale.co>
2026-02-01 09:54:36 +0530

    dark theme fixes

--- src/components/CharecterCard.tsx
+++ src/components/CharecterCard.tsx
@@ -10,7 +10,7 @@ function characterCard({ id, name, images = [] }: CharacterProps) {
   return (
     
     <a href={"/characters/" + id}>
-      <div className={`items-center ${images.length==0? "content-center":"" }   shadow-lg  bg-white cursor-pointer  hover:scale-105 rounded-lg  lg:w-56 w-80 bg-whiteo  h-56   mx-auto`}>
+      <div className={`items-center ${images.length==0? "content-center":"" }   shadow-lg  bg-white cursor-pointer  hover:scale-102 rounded-lg  lg:w-56 w-80 bg-whiteo  h-56   mx-auto`}>
         {images?.length > 0 && swipeImage(images)}
         <h1 className="font-semibold  mb- p-2 text-center my-auto text-black">
           {name}
